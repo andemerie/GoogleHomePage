a56c3da5ef7ee3b914ca566ad278748409eaf09a
Victoria Drozd <victoriadroalex@tut.by>
2017-05-12 00:02:57 +0300

    add settings menu

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ search_line.addEventListener("blur", function () {
     this.classList.remove('focused');
     this.classList.add('usual');
 }, true);
+
+var settings = document.getElementById('settings');
+
+settings.onclick = function(event) {
+    var settings_menu = document.getElementById('settings_menu');
+    settings_menu.style.display = 'block';
+}
+
+var body = document.querySelector('body');
+
+body.onclick = function (event) {
+    var target = event.target;
+
+    if (target.id == "settings") return;
+
+    var settings_menu = document.getElementById('settings_menu');
+    settings_menu.style.display = 'none';
+}
